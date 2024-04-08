d1b347111cfd15634cc605f6dc11517038aba378
KristyMoussally <2242744@cmontmorency.qc.ca>
2024-04-08 13:24:51 -0400

    autre changement (doit etre responsive)

--- script/script.js
+++ script/script.js
@@ -2,15 +2,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 });
 
+GSDevTools.create();
+
 gsap.fromTo(".fleche", { y: 10 }, { y: 30, repeat: -1, yoyo: true });
 
 let animYeux = document.querySelector(".debut");
+let animGhost = document.querySelector("#ghost");
 
 window.addEventListener("scroll", () => {
   setTimeout(() => {
     animYeux.style.animationPlayState = "paused";
+    animGhost.style.animationPlayState = "paused";
     document.querySelector("body").classList.remove("is-scrolling");
   }, 100);
   animYeux.style.animationPlayState = "running";
+  animGhost.style.animationPlayState = "running";
   document.querySelector("body").classList.add("is-scrolling");
 });
