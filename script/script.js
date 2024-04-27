f96673fb2f41ceade0384355a971b4716430900b
KristyMoussally <2242744@cmontmorency.qc.ca>
2024-04-27 02:55:56 -0400

    Resized all images (need text)

--- script/script.js
+++ script/script.js
@@ -2,16 +2,19 @@ gsap.fromTo(".fleche", { y: 10 }, { y: 30, repeat: -1, yoyo: true });
 
 let animYeux = document.querySelector(".debut");
 let animGhost = document.querySelector("#ghost");
+let animGhost2 = document.querySelector("#ghost-big");
 
 window.addEventListener("scroll", () => {
   setTimeout(() => {
     animYeux.style.animationPlayState = "paused";
     animGhost.style.animationPlayState = "paused";
+    animGhost2.style.animationPlayState = "paused";
     document.querySelector("body").classList.remove("is-scrolling");
   }, 100);
   document.querySelector("body").classList.add("is-scrolling");
   animYeux.style.animationPlayState = "running";
   animGhost.style.animationPlayState = "running";
+  animGhost2.style.animationPlayState = "running";
 });
 
 /*
